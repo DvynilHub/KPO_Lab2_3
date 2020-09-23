ad00c657d45f466797a1db22b5791028ef5d0447
DvynilHub <belonogov220@gmail.com>
2020-09-23 18:14:53 +0800

    Рабочая версия

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,81 @@
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+    public static void main(String args[]) throws IOException {
 
-    // построчное считывание файла
-    public static void main(String[] args) {
+
+
+        trans(matr());
+
+
+    }
+    private static void trans(double[][] a){  //метод транспонирования и вывода матрицы
         try {
-            File file = new File("C:\\Users\\User\\Desktop\\123.txt");
-            //создаем объект FileReader для объекта File
-            FileReader fr = new FileReader(file);
-            //создаем BufferedReader с существующего FileReader для построчного считывания
-            BufferedReader reader = new BufferedReader(fr);
-            // считаем сначала первую строку
-            String line = reader.readLine();
-            int k = line.length()/2;
-            int m[][] = new int[k][k];
-            int h = 1;
-            for (int i=0;i<k;i++){
-                m[0][i] = Character.getNumericValue(line.charAt(h));
-                h+=2;
-            }
-            System.out.println(m[0][0]);
-            System.out.println("--------------");
-            int l = 1;
-            while (line != null) {
-
-
-                // считываем остальные строки в цикле
-                line = reader.readLine();
-                int o = 1;
-                for (int i=0;i<k;i++){
-                    m[l][i] = Character.getNumericValue(line.charAt(o));
-                    o+=2;
+            for (int i = 0; i < a.length; i++) {
+                for (int j = i + 1; j < a.length; j++) {
+                    double temp = a[i][j];
+                    a[i][j] = a[j][i];
+                    a[j][i] = temp;
                 }
-                l++;
             }
-            for (int i = 0;i<k;i++){
-                for (int j = 0;j<k;j++){
-                    System.out.print(m[i][j]+" ");
+            System.out.println();
+            System.out.println("Новая матрица");
+            System.out.println("------");
+            for (double[] doubles : a) {
+                for (int j = 0; j < a.length; j++) {
+                    System.out.print(doubles[j] + " ");
                 }
                 System.out.println();
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Матрицу невозможно транспонировать");
+        } catch (NullPointerException e){
+            System.out.println("Неверный формат матрицы");
         }
+
     }
-}
+    private static double[][] matr() throws IOException { //функция считывания матрицы из файла
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\User\\Desktop\\123.txt"));
+        try {
+            List<String> lines = new ArrayList<>();
+            while (br.ready()) {
+                lines.add(br.readLine());
+            }
+            int matrixWidth = lines.get(0).split(" ").length;
+            int matrixHeight = lines.size();
+            if (matrixHeight != matrixWidth){
+                return null;
+            }
+
+            double[][] matrix = new double[matrixHeight][matrixWidth];
+
+            for (int i = 0; i < matrixHeight; i++) {
+                for (int j = 0; j < matrixWidth; j++) {
+                    String[] line = lines.get(i).split(" ");
+                    matrix[i][j] = Double.parseDouble(line[j]);
+                }
+            }
+            for (int i = 0; i < matrixHeight; i++) {
+                for (int j = 0;j<matrixWidth;j++){
+                    System.out.print(matrix[i][j]+" ");
+                }
+                System.out.println();
+
+            }
+            return matrix;
+        } catch (IOException e){
+            e.printStackTrace();
+            return null;
+        } catch (NumberFormatException e){
+            System.out.println("Неверный формат элементов массива...");
+            return null;
+        }
+
+
+    }
+
+    }
